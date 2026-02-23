ea88d665cbdb8098789f99d1bf5c9605b94c59d6
Zakhar <zz824985@gmail.com>
2026-02-23 11:14:37 -0800

    commit

--- src/main/java/ForMyProjectPages/CompleteWebForm.java
+++ src/main/java/ForMyProjectPages/CompleteWebForm.java
@@ -5,13 +5,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
+
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.Random;
+
 
 public class CompleteWebForm extends BasePage{
     private final By firstNameField = By.id("first-name");
